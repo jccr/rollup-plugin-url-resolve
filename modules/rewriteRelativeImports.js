1e1b4e49ce91419e918090cfdc3af083a4832952
Juan Carlos Corona Romero <c@rlos.email>
2025-03-07 01:02:56 -0800

    Match `text/javascript` content type for rewrite
    The latest spec for HTML and RFC 9239 calls for using `text/javascript` over other content types.
    
    My motivation for this change came from importing a script served by a CDN that used this content type. 
    I faced an error with resolving relative imports:
    
    ```
    Error: Could not resolve '../../<relative path>' from https://<cdn with js>
    ```

--- modules/rewriteRelativeImports.js
+++ modules/rewriteRelativeImports.js
@@ -2,6 +2,7 @@ import rewriteRelativeJavaScriptImports from './rewriteRelativeJavaScriptImports
 
 export default function rewriteRelativeImports(base, contentType, code) {
   switch (contentType) {
+    case 'text/javascript':
     case 'application/javascript':
       return rewriteRelativeJavaScriptImports(base, code);
     default:
